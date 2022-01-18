85196aed6f4d8ba5501da7da0be2bc2449875062
Rapkowsky <robertrapkowski19@gmail.com>
2022-01-19 00:56:07 +0100

    Section about-us added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const navElement = document.querySelector(".navbar");
 
-
 	function addShadow() {
 		if (window.scrollY >= 150) {
 			navElement.classList.add("shadow-bg");
