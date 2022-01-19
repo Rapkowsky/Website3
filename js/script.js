27675dd2c73f1fd66ee4e29427630859da3d4724
Rapkowsky <robertrapkowski19@gmail.com>
2022-01-19 20:01:42 +0100

    about-us hover + collapsing menu list

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const navElement = document.querySelector(".navbar");
+	const allNavItems = document.querySelectorAll(".nav-link");
+	const navListCollapse = document.querySelector(".navbar-collapse");
 
 	function addShadow() {
-		if (window.scrollY >= 150) {
+		if (window.scrollY >= 10) {
 			navElement.classList.add("shadow-bg");
 		} else navElement.classList.remove("shadow-bg");
 	}
+
+	allNavItems.forEach((item) =>
+		item.addEventListener("click", () => {
+			navListCollapse.classList.remove("show");
+		})
+	);
+
 	window.addEventListener("scroll", addShadow);
 });
